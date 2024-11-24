65f6ec7a5d677c71afdab6639107ee2cc67ceb65
rosemqa <ers6wobmo09m@mail.ru>
2024-11-24 23:47:25 +0300

    added login_locators.py

---
+++ locators/login_locators.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+
+
+class LoginLocators:
+    USER_NAME = (By.CSS_SELECTOR, '#username')
+    PASSWORD = (By.CSS_SELECTOR, '#password')
+    LOG_IN_BTN = (By.CSS_SELECTOR, '[type="submit"][name="action"]')
+    SHOW_PASSWORD_BTN = (By.CSS_SELECTOR, '[role="switch"]')
+    SHOW_PASSWORD_TOOLTIP = (By.CSS_SELECTOR, 'span.show-password-tooltip')
+    HIDE_PASSWORD_TOOLTIP = (By.CSS_SELECTOR, 'span.hide-password-tooltip')
+    FORGOT_PASSWORD_LINK = (By.CSS_SELECTOR, '[method="POST"] p a')
+    SIGN_UP_LINK = (By.CSS_SELECTOR, '.ulp-alternate-action a')
+    USERNAME_REQUIRED_ERROR = (By.CSS_SELECTOR, '#error-cs-username-required')
+    EMAIL_INVALID_ERROR = (By.CSS_SELECTOR, '#error-cs-email-invalid')
+    PASSWORD_REQUIRED_ERROR = (By.CSS_SELECTOR, '#error-cs-password-required')
+    WRONG_CREDENTIALS = (By.CSS_SELECTOR, '#error-element-password')
+
