bcaccf47d970e6d6d6aa5966491dd17f03e0e135
rosemqa <ers6wobmo09m@mail.ru>
2024-12-03 00:39:32 +0300

    fixed the locator for email error on login page

--- data/constants.py
+++ data/constants.py
@@ -1,6 +1,6 @@
 
 class Login:
-    USERNAME_REQUIRED_ERROR_TEXT = 'Username is required'
+    EMAIL_REQUIRED_ERROR_TEXT = 'Please enter an email address'
     PASSWORD_REQUIRED_ERROR_TEXT = 'Password is required'
     EMAIL_INVALID_ERROR_TEXT = 'Email is not valid.'
     WRONG_CREDENTIALS_ERROR_TEXT = 'Wrong email or password'
