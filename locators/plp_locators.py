cbbe014c71ed6a3eb825556a6c67fb382b022dac
rosemqa <ers6wobmo09m@mail.ru>
2024-12-15 00:54:30 +0300

    added locators in plp_locators.py

--- locators/plp_locators.py
+++ locators/plp_locators.py
@@ -7,9 +7,15 @@ class PLPLocators:
     PRODUCT_COLOR = (By.CSS_SELECTOR, '[data-locator-id*="plp-productColour"]')
     PRODUCT_PRICE = (By.CSS_SELECTOR, '[class*="product-card_product-price"]')
     PRODUCT_SIZE = (By.CSS_SELECTOR, '[data-locator-id*="plp-size"]')
+    PRODUCT_TAG_NEW = (By.CSS_SELECTOR, '[data-locator-id="plp-productTag-new-read"]')
+
     CLEAR_ALL_FILTERS = (By.CSS_SELECTOR, '[data-locator-id="filtersPanel-clearAll-select"]')
     SORT_BY_PRICE_ASC = (By.CSS_SELECTOR, '[data-locator-id="filtersPanel-filterOptionLabel-SORTLTH-select"]')
     SORT_BY_PRICE_DESC = (By.CSS_SELECTOR, '[data-locator-id="filtersPanel-filterOptionLabel-SORTHTL-select"]')
-    PRODUCT_TAG_NEW = (By.CSS_SELECTOR, '[data-locator-id="plp-productTag-new-read"]')
+    SORT_BY_NEWEST = (By.CSS_SELECTOR, '[data-locator-id="filtersPanel-filterOptionLabel-NEWEST-select"]')
+    PRICE_FILTER_DROPDOWN = (By.CSS_SELECTOR, '[data-locator-id="filtersPanel-filterCategory-PRICE-select"]')
+    PRICE_FILTER_ITEM = (By.CSS_SELECTOR, '[for*="price-"]')
+
     LOAD_MORE_BTN = (By.CSS_SELECTOR, 'a[class*="button_button"]')
-    
+    VIEW_ALL_BTN = (By.CSS_SELECTOR, '[class*="pagination_view-all"]')
+    PAGINATION_TEXT = (By.CSS_SELECTOR, '[class*="pagination_pagination-text"]')
