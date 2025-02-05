e3817d1caf3b86484bc08ec130f31ed553578b04
rosemqa <ers6wobmo09m@mail.ru>
2025-02-05 23:23:09 +0300

    updated the locator for email error on login page

--- data/constants.py
+++ data/constants.py
@@ -2,7 +2,7 @@ from data.data import AutData
 
 
 class Login:
-    EMAIL_REQUIRED_ERROR_TEXT = 'Please enter an email address'
+    EMAIL_REQUIRED_ERROR_TEXT = 'Username is required'
     PASSWORD_REQUIRED_ERROR_TEXT = 'Password is required'
     EMAIL_INVALID_ERROR_TEXT = 'Email is not valid.'
     WRONG_CREDENTIALS_ERROR_TEXT = 'Wrong email or password'
--- locators/login_locators.py
+++ locators/login_locators.py
@@ -10,7 +10,7 @@ class LoginLocators:
     HIDE_PASSWORD_TOOLTIP = (By.CSS_SELECTOR, 'span.hide-password-tooltip')
     FORGOT_PASSWORD_LINK = (By.CSS_SELECTOR, '[method="POST"] p a')
     SIGN_UP_LINK = (By.CSS_SELECTOR, '.ulp-alternate-action a')
-    EMAIL_REQUIRED_ERROR = (By.CSS_SELECTOR, '#error-cs-email-required')
+    EMAIL_REQUIRED_ERROR = (By.CSS_SELECTOR, '#error-cs-username-required')
     EMAIL_INVALID_ERROR = (By.CSS_SELECTOR, '#error-cs-email-invalid')
     PASSWORD_REQUIRED_ERROR = (By.CSS_SELECTOR, '#error-cs-password-required')
     WRONG_CREDENTIALS = (By.CSS_SELECTOR, '#error-element-password')
