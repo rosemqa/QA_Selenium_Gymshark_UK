2d96aec213e4c56fa5d2d73ed80ed8ebe803d582
rosemqa <ers6wobmo09m@mail.ru>
2025-02-13 20:22:39 +0300

    added more methods in base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,11 @@
 import time
-
 import allure
 from selenium.common import TimeoutException
-from selenium.webdriver import ActionChains
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from locators.base_locators import BaseLocators
 
 
 class BasePage:
@@ -15,12 +16,19 @@ class BasePage:
     def open_page(self):
         with allure.step(f'Open {self.url} page'):
             self.driver.get(self.url)
+            time.sleep(2)
+            if self.is_element_present(BaseLocators.GEOLOCATION_CONFIRMATION_CLOSE_BTN, 1):
+                self.close_geolocation_confirmation()
+            if self.is_element_present(BaseLocators.CLOSE_COOKIES_BTN, 1):
+                self.close_cookies_banner()
+                WebDriverWait(self.driver, 2) \
+                    .until(EC.invisibility_of_element_located(BaseLocators.CLOSE_COOKIES_BTN))
 
-    def is_open(self, timeout=10):
+    def is_open(self, timeout=5):
         with (allure.step(f'Page {self.url} is open')):
             WebDriverWait(self.driver, timeout) \
                 .until(EC.url_to_be(self.url),
-                       message='Expected url {self.url} is not open, actual url {self.driver.current_url}')
+                       message=f'Expected url {self.url} is not open, actual url {self.driver.current_url}')
 
     def find_element(self, locator, timeout=10):
         return WebDriverWait(self.driver, timeout)\
@@ -59,8 +67,71 @@ class BasePage:
         element = self.find_element(locator)
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
 
+    @allure.step('Select item in drop-down list by visible text')
+    def select_item_in_dropdown_by_text(self, locator, text):
+        """Select item in drop-down list by visible text"""
+        Select(self.find_element(locator)).select_by_visible_text(text)
+        return text
+
+    @allure.step('Select item in drop-down list by index, get the selected element text')
+    def select_item_in_dropdown_by_index(self, locator, index):
+        """Select item in drop-down list by index"""
+        select = Select(self.find_element(locator))
+        select.select_by_index(index)
+        element_text = select.first_selected_option.text
+        return element_text
+
     def move_mouse_by_offset(self, x_offset, y_offset):
         action = ActionChains(self.driver)
         action \
             .move_by_offset(x_offset, y_offset) \
             .perform()
+
+    def close_geolocation_confirmation(self):
+        self.find_element(BaseLocators.GEOLOCATION_CONFIRMATION_CLOSE_BTN).click()
+
+    @allure.step('Close the Cookies banner')
+    def close_cookies_banner(self):
+        self.find_element(BaseLocators.CLOSE_COOKIES_BTN).click()
+
+    @allure.step('Get the "logged-in" attribute of account icon')
+    def check_account_icon(self):
+        account = self.find_element(BaseLocators.ACCOUNT_NAME)
+        account_attribute = account.get_attribute('data-user-logged-in')
+        if account_attribute == "true":
+            return True, account.text
+        elif account_attribute == 'false':
+            return False, account.text
+
+    # ACTIONS
+    @allure.step('Get text in the Sign In tooltip')
+    def get_sign_in_tooltip_text(self):
+        return self.find_element(BaseLocators.SIGN_IN_TOOLTIP).text
+
+    @allure.step('Click the Bag icon in the header')
+    def open_mini_bag(self):
+        self.find_element(BaseLocators.BAG_ICON).click()
+
+    @allure.step('Check if the bag count icon present')
+    def is_bag_count_icon_present(self):
+        bag_count_icon = BaseLocators.BAG_COUNT
+        if self.is_element_present(bag_count_icon):
+            count_value = int(self.find_element(bag_count_icon).text)
+            return True, count_value
+        return False, None
+
+    @allure.step('Check if the wishlist toast present')
+    def is_wishlist_toast_present(self):
+        return self.is_element_present(BaseLocators.WISHLIST_TOAST)
+
+    @allure.step('Check if the wishlist toast disappears after 5 seconds')
+    def is_wishlist_toast_disappeared(self):
+        return self.is_disappeared(BaseLocators.WISHLIST_TOAST, 6)
+
+    @allure.step('Check if the Sign In tooltip present')
+    def is_sign_in_tooltip_present(self):
+        return self.is_element_present(BaseLocators.SIGN_IN_TOOLTIP)
+
+    @allure.step('Check if the Sign In tooltip disappears after 5 seconds')
+    def is_sign_in_tooltip_disappeared(self):
+        return self.is_disappeared(BaseLocators.SIGN_IN_TOOLTIP, 6)
